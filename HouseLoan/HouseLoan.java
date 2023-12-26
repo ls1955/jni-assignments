123187bcba8df18d21cbb69afbcfc0ada91a0ef5
ls1955 <xxlsxx1955@gmail.com>
2023-12-26 13:14:14 +0800

    Include initial HouseLoan draft

---
+++ HouseLoan/HouseLoan.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class HouseLoan {
+    private native float monthlyPayment(int loanAmount, float interestRate, int paymentPerYear, int numberOfYears);
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter loan amount");
+        int loanAmount = Integer.parseInt(scan.nextLine());
+        System.out.println("Enter interest rate");
+        float interestRate = Float.parseFloat(scan.nextLine());
+        System.out.println("Enter payment per year");
+        int paymentPerYear = Integer.parseInt(scan.nextLine());
+        System.out.println("Enter number of year");
+        int numberOfYears = Integer.parseInt(scan.nextLine());
+
+        float monthlyPayment = new HouseLoan().calculateMonthyPayment(
+            loanAmount, interestRate, paymentPerYear, numberOfYears;
+        )
+
+        System.out.println("Monthly payment: " + monthlyPayment);
+    }
+}
