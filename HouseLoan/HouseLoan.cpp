8d8bcff523fe4f6575ce87513a63be0eca8b3f5f
Cheong <xxlsxx1955@gmail.com>
2023-12-27 15:48:49 +0800

    Implement Java_HouseLoan_calculatePrincipalPayment
    Again, hasn't test it yet. Now that author think about it, there is a
    Xubuntu sitting inside this device that he is typing on. What is he
    doing with he life?

--- HouseLoan/HouseLoan.cpp
+++ HouseLoan/HouseLoan.cpp
@@ -18,3 +18,27 @@ JNIEXPORT jfloat JNICALL Java_HouseLoan_calculateMonthlyPayment(
 
     return ((jfloat) loanAmount * (r / n)) / (1 - pow((1 + (r / n)), (-1 * n * t)));
 }
+
+JNIEXPORT void JNICALL Java_HouseLoan_printAmortizationTable(
+    JNIEnv *env,
+    jobject obj,
+    jint loanAmount,
+    jfloat interestRate,
+    jfloat montlyPayment
+) {
+    cout << "Goodbye, world" << endl;
+};
+
+JNIEXPORT jfloat JNICALL Java_HouseLoan_calculatePrincipalPayment(
+    JNIEnv *env,
+    jobject obj,
+    jint loanAmount,
+    jfloat interestRate,
+    jfloat montlyPayment
+) {
+    jfloat mp = montlyPayment;
+    jfloat olb = (jfloat) loanAmount;
+    jint monthsAmount = 12;
+
+    return mp - (olb * (interestRate / monthsAmount));
+};
