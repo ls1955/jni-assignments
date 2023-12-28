b586de09e039589f78e3d3b1da5b45564d019ca5
Cheong <xxlsxx1955@gmail.com>
2023-12-28 08:47:12 +0800

    Round values to 2 decimal places
    An attempt to see if doing this is sufficient for calculating correct result.

--- HouseLoan/HouseLoan.cpp
+++ HouseLoan/HouseLoan.cpp
@@ -1,4 +1,5 @@
 #include <jni.h>
+#include <cmath>
 #include <math.h>
 #include <iostream>
 #include "HouseLoan.h"
@@ -16,7 +17,10 @@ JNIEXPORT jfloat JNICALL Java_HouseLoan_calculateMonthlyPayment(
     jfloat n = (jfloat) paymentPerYear;
     jfloat t = (jfloat) numberOfYears;
 
-    return ((jfloat) loanAmount * (r / n)) / (1 - pow((1 + (r / n)), (-1 * n * t)));
+    jfloat result = ((jfloat) loanAmount * (r / n)) / (1 - pow((1 + (r / n)), (-1 * n * t)));
+
+    // round to 2 decimal places as given output is in 2 decimal places
+    return round(result * 100.0) / 100.0;
 }
 
 JNIEXPORT void JNICALL Java_HouseLoan_printAmortizationTable(
@@ -35,6 +39,9 @@ JNIEXPORT void JNICALL Java_HouseLoan_printAmortizationTable(
     // get method ID for *calculatePrinciplePayment* to call it later
     jmethodID pCallback = env->GetMethodID(klass, "calculatePrincipalPayment", "(IFF)F");
 
+    // round to 2 decimal as calculation seems off without it
+    interestRate = round(interestRate * 100.0) / 100.0;
+    // cast loanAmount to jfloat as claculation seems off without it
     jfloat balance = (jfloat) loanAmount;
 
     for (int i = 1 ; i <= 12; i++) {
@@ -42,6 +49,11 @@ JNIEXPORT void JNICALL Java_HouseLoan_printAmortizationTable(
             obj, pCallback, (jint) balance, interestRate, montlyPayment
         );
         balance -= principal;
+
+        // round all values to 2 decimal places
+        principal = round(principal * 100.0) / 100.0;
+        balance = round(balance * 100.0) / 100.0;
+
 
         // TODO: Calculate the interest, maybe include another native method?
         cout << "|  " << i << " |  " << montlyPayment << " | " << principal << " | " << balance << endl;
