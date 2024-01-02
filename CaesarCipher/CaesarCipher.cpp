e8bed954b5d848d114e13372b2724e559bb686ee
Cheong <xxlsxx1955@gmail.com>
2024-01-02 21:43:05 +0800

    Implement native methods
    As always, hasn't tested them though.

---
+++ CaesarCipher/CaesarCipher.cpp
@@ -0,0 +1,57 @@
+#include <jni.h>
+#include <iostream>
+#include "CaesarCipher.h"
+using namespace std;
+
+JNIEXPORT jstring JNICALL Java_CaesarCipher_getInput(JNIEnv *env, jobject obj) {
+    string input;
+    cout << "Enter your input: ";
+    cin >> input;
+
+    // convert input to C++ String to C String, then to jstring
+    return env->NewStringUTF(input.c_str());
+};
+
+JNIEXPORT jstring JNICALL Java_CaesarCipher_encrypt(
+    JNIEnv *env,
+    jobject obj,
+    jstring plaintext,
+    jint key
+) {
+    const char *plaintextStr = env->GetStringUTFChars(plaintext, NULL);
+    string result;
+
+    for (char chr : plaintextStr) {
+        if (chr >= 'A' && chr <= 'Z') {
+            result += ((chr - 'A' + key) % 26 + 'A');
+        } else if (chr >= 'a' && chr <= 'z') {
+            result += ((chr - 'a' + key) % 26 + 'a');
+        } else {
+            result += chr;
+        }
+    }
+
+    return env->NewStringUTF(result.c_str());
+};
+
+JNIEXPORT jstring JNICALL Java_CaesarCipher_decrypt(
+    JNIEnv *env,
+    jobject obj,
+    jstring ciphertext,
+    jint key
+) {
+    const char *plaintextStr = env->GetStringUTFChars(plaintext, NULL);
+    string result;
+
+    for (char chr : plaintextStr) {
+        if (chr >= 'A' && chr <= 'Z') {
+            result += ((chr - 'A' - key) % 26 + 'A');
+        } else if (chr >= 'a' && chr <= 'z') {
+            result += ((chr - 'a' - key) % 26 + 'a');
+        } else {
+            result += chr;
+        }
+    }
+
+    return env->NewStringUTF(result.c_str());
+};
