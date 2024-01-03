c327f526a30ec3a4b28bfef075cefc178a376585
Cheong <xxlsxx1955@gmail.com>
2024-01-03 11:54:18 +0800

    Fix comment
    .bat have different comment than .sh

--- Q2/Q2.bat
+++ Q2/Q2.bat
@@ -3,5 +3,5 @@
 javac -h . Q2.java
 x86_64-w64-mingw32-g++ -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" -shared -o Q2C.dll Q2.cpp
 
-# substitute name and id here
+@REM substitute name and id here
 java -Djava.library.path=. Q2 tuna 1112223339
