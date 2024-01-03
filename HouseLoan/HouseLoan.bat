bb9ea80625d5de25856dd264ab296822de3adcc4
Cheong <xxlsxx1955@gmail.com>
2024-01-03 11:46:45 +0800

    Fix wrong compiler issue
    Seems like in Window, g++ is required instead of gcc, else C++ standard
    library could not be found.

--- HouseLoan/HouseLoan.bat
+++ HouseLoan/HouseLoan.bat
@@ -1,6 +1,6 @@
 @echo off
 
 javac -h . HouseLoan.java
-x86_64-w64-mingw32-gcc -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" -shared -o HouseLoan.dll HouseLoan.cpp
+x86_64-w64-mingw32-g++ -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" -shared -o HouseLoan.dll HouseLoan.cpp
 
 java -Djava.library.path=. HouseLoan
