39f2d7843fefdd90767548d2990b9480089de4a3
ls1955 <xxlsxx1955@gmail.com>
2024-01-21 22:15:00 +0800

    Fix wrong filename

--- CaesarCipher/CaesarCipher.bat
+++ CaesarCipher/CaesarCipher.bat
@@ -1,6 +1,6 @@
 @echo off
 
-javac -h . Q2.java
+javac -h . CaesarCipher.java
 x86_64-w64-mingw32-g++ -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" -shared -o CaesarCipher.dll CaesarCipher.cpp
 
 # substitute name and id here
